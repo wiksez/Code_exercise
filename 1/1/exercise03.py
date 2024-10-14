2462ca3ad5309c194f9286606621e0021ce18068
wiksez <moderviktor@gmail.com>
2024-10-14 12:18:28 +0200

    Working with strings

---
+++ 1/1/exercise03.py
@@ -0,0 +1,5 @@
+print("I'm a student and I'll become a \"strong\" programmer") #I'm a student and I'll become a "strong" programmer
+print(r"C:\Users\GoodStudent") #C:\Users\GoodStudent
+x = "My name is Agent Smith"
+print(x[1]) #y
+print(x[3:16:3]) #nesgt
